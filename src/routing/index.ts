28fd7d645ed35923e6d652b6c30853a5b5c4d900
Mathieu-R <mathieu@igloo.be>
2024-11-05 10:53:08 +0100

    add default options for routing

--- src/routing/index.ts
+++ src/routing/index.ts
@@ -1,12 +1,21 @@
 import { Provider } from "../providers";
-import { RoutingOptions } from "../types/common";
+import { RoutingOptions, TransportMode } from "../types/common";
 import { HereRouting } from "./here";
 import { MapboxRouting } from "./mapbox";
+
+const DEFAULT_OPTIONS = {
+  transportMode: "car" as TransportMode,
+  alternatives: false,
+  traffic: false,
+  renderPath: false,
+};
 
 export async function routing(
   provider: `${Provider}`,
   options: RoutingOptions,
 ) {
+  options = {...DEFAULT_OPTIONS, ...options}
+
   if (provider === Provider.HERE) {
     return HereRouting(options);
   }
